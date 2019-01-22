dbcd855c1a22eb7e1490b27cc46547d5c77b8f9b
Saint-Jean Germila <germila@noukod.com>
2019-01-22 06:44:36 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
+    #DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
   end
 
